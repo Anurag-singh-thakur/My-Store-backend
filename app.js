51f596184cd5525ae729d2b71e8d789754468af7
Sagar Seth <lightningsagar0@gmail.com>
2024-12-21 01:33:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/db.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./route/userRoute.js";
+import productRoute from "./route/productRoute.js";
+import { v2 as cloudinary } from "cloudinary";
+import path from "path";
+const app = express()
+dotenv.config();
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+    timeout: 6000000
+});
+
+
+connectDB();
+const _dirname = path.resolve();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
+app.use('/api/user', userRoute);
+app.use('/api/p', productRoute);
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`http://localhost:${port}`));
+
