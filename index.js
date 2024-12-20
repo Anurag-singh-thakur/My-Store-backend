37c4e837288b235e6b007ecced99cc975b33be7e
Sagar Seth <lightningsagar0@gmail.com>
2024-12-21 05:17:47 +0530

    updated the index.js

--- index.js
+++ index.js
@@ -7,21 +7,25 @@ import productRoute from "./route/productRoute.js";
 import { v2 as cloudinary } from "cloudinary";
 import path from "path";
 import cors from "cors";
+
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:3000", "http://my-store-frontend-chi.vercel.app/"],   
-    methods: ["GET", "POST", "PUT", "DELETE"], 
-    allowedHeaders: ["Content-Type", "Authorization"],  
-    credentials: true,  
+  origin: ["http://localhost:3000", "https://my-store-frontend-chi.vercel.app","http://my-store-frontend-chi.vercel.app"],   
+  methods: ["GET", "POST", "PUT", "DELETE"],  
+  allowedHeaders: ["Content-Type", "Authorization"],   
+  credentials: true,  
+  preflightContinue: true, 
 }));
+
+  
 
 dotenv.config();
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
-    timeout: 6000000,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+  timeout: 6000000,
 });
 
 connectDB();
@@ -29,9 +33,15 @@ const _dirname = path.resolve();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use((req, res, next) => {
+    console.log("CORS headers being sent");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
 app.get('/', (req, res) => {
-    res.send("is Working");
+  res.send("is Working");
 });
 app.use('/api/user', userRoute);
 app.use('/api/p', productRoute);
